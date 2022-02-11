65517a4713458436006665277c30aaac9b9cd649
Kevin Caccamo <kevin@ciinet.org>
2022-02-11 02:56:38 -0500

    Use Cursor to read the data in read_texturex
    Also, remove copy_into_array

--- doom-wad/src/res/textures.rs
+++ doom-wad/src/res/textures.rs
@@ -1,9 +1,8 @@
 // TEXTURE1, TEXTURE2, and PNAMES
 use crate::wad;
-use crate::util;
 use crate::wad::util::*;
 use std::error::Error;
-use std::io::{Cursor, Read};
+use std::io::{Cursor, Read, Seek, SeekFrom};
 
 pub struct TexturePatch {
 	patch: String,
@@ -36,10 +35,11 @@ fn read_pnames(pnames: &wad::DoomWadLump) ->
 		i32::from_le_bytes(num_buffer) as usize
 	};
 	let mut names: Vec<String> = Vec::with_capacity(name_count);
-	for name_index in 0..name_count {
+	(0..name_count).map(|_| -> Result<(), Box<dyn Error>> {
 		pos.read_exact(&mut name_buffer)?;
 		names.push(String::from_utf8(lump_name(&name_buffer))?);
-	}
+		Ok(())
+	}).collect::<Result<(), Box<dyn Error>>>()?;
 	Ok(names)
 }
 
@@ -47,36 +47,39 @@ pub fn read_texturex<'a>(wad: &'a wad::DoomWad, list: &'a wad::DoomWadLump, pnam
 	Result<TextureDefinitions<'a>, Box<dyn Error>>
 {
 	let patches = read_pnames(pnames)?;
-	let mut pos: usize = 0;
+	let mut pos = Cursor::new(&list.data);
+	let mut name_buffer: [u8; 8] = [0; 8];
 	let mut num_buffer: [u8; 4] = [0; 4];
+	let mut short_buffer: [u8; 2] = [0; 2];
 	let count: usize = {
-		util::copy_into_array(&mut num_buffer, &list.data, pos);
+		pos.read_exact(&mut num_buffer)?;
 		i32::from_le_bytes(num_buffer) as usize
 	};
-	let mut tex_def_pos: Vec<usize> = Vec::with_capacity(count);
-	for _texture_index in 0..count {
-		util::copy_into_array(&mut num_buffer, &list.data, pos);
-		tex_def_pos.push(u32::from_le_bytes(num_buffer) as usize);
-	}
-	let mut name_buffer: [u8; 8] = [0; 8];
-	let mut short_buffer: [u8; 2] = [0; 2];
+	let mut tex_def_pos: Vec<u64> = vec![0; count];
+	(0..count).map(|index| -> Result<(), Box<dyn Error>> {
+		pos.read_exact(&mut num_buffer)?;
+		tex_def_pos[index] = u32::from_le_bytes(num_buffer) as u64;
+		Ok(())
+	}).collect::<Result<(), Box<dyn Error>>>()?;
 	let mut defs = TextureDefinitions {
 		textures: Vec::with_capacity(count),
 		lump: list,
 		// by_name: HashMap::new(),
 	};
-	for texture_pos in tex_def_pos.into_iter() {
-		pos = texture_pos;
-		pos = util::copy_into_array(&mut name_buffer, &list.data, pos);
+	tex_def_pos.into_iter().try_for_each(|offset| -> Result<(), Box<dyn Error>> {
+		pos.seek(SeekFrom::Start(offset))?;
+		// Name (8 bytes)
+		pos.read_exact(&mut name_buffer)?;
 		let name = String::from_utf8(lump_name(&name_buffer))?;
-		pos = util::copy_into_array(&mut num_buffer, &list.data, pos);
+		// Flags (4 bytes)
+		pos.read_exact(&mut num_buffer)?;
 		let flags = i32::from_le_bytes(num_buffer);
-		pos = util::copy_into_array(&mut short_buffer, &list.data, pos);
+		pos.read_exact(&mut short_buffer)?;
 		let width = u16::from_le_bytes(short_buffer);
-		pos = util::copy_into_array(&mut short_buffer, &list.data, pos);
+		pos.read_exact(&mut short_buffer)?;
 		let height = u16::from_le_bytes(short_buffer);
-		pos += 4; // skip columndirectory
-		pos = util::copy_into_array(&mut short_buffer, &list.data, pos);
+		pos.seek(SeekFrom::Current(4))?; // skip columndirectory
+		pos.read_exact(&mut short_buffer)?;
 		let patch_count = u16::from_le_bytes(short_buffer);
 		let mut texture = Texture {
 			name: name.clone(),
@@ -85,14 +88,14 @@ pub fn read_texturex<'a>(wad: &'a wad::DoomWad, list: &'a wad::DoomWadLump, pnam
 			height: height,
 			patches: Vec::new(),
 		};
-		for _patch_index in 0..patch_count {
-			pos = util::copy_into_array(&mut short_buffer, &list.data, pos);
+		(0..patch_count ).map(|_| -> Result<(), Box<dyn Error>> {
+			pos.read_exact(&mut short_buffer)?;
 			let x = i16::from_le_bytes(short_buffer);
-			pos = util::copy_into_array(&mut short_buffer, &list.data, pos);
+			pos.read_exact(&mut short_buffer)?;
 			let y = i16::from_le_bytes(short_buffer);
-			pos = util::copy_into_array(&mut short_buffer, &list.data, pos);
+			pos.read_exact(&mut short_buffer)?;
 			let patch_name = patches[u16::from_le_bytes(short_buffer) as usize].clone();
-			pos = util::copy_into_array(&mut num_buffer, &list.data, pos);
+			pos.read_exact(&mut num_buffer)?;
 			let flags = i32::from_le_bytes(num_buffer);
 			let patch = TexturePatch {
 				patch: patch_name,
@@ -101,8 +104,10 @@ pub fn read_texturex<'a>(wad: &'a wad::DoomWad, list: &'a wad::DoomWadLump, pnam
 				flags: flags
 			};
 			texture.patches.push(patch);
-		}
+			Ok(())
+		}).collect::<Result<(), Box<dyn Error>>>()?;
 		defs.textures.push(texture);
-	}
+		Ok(())
+	})?;
 	Ok(defs)
 }
