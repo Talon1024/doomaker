79f89368d0eee332fca9afc46e6c9c1b0b8e3dba
Kevin Caccamo <kevin@ciinet.org>
2022-02-13 00:31:29 -0500

    Remove some repeated code
    Do the multiplication after the block

--- map-to-3D/src/sectorpolygonbuilder.rs
+++ map-to-3D/src/sectorpolygonbuilder.rs
@@ -59,9 +59,7 @@ fn angle_between(
 		cfg_if::cfg_if! {
 			if #[cfg(atan_angle_calc)] {
 				// Based on http://www.euclideanspace.com/maths/algebra/vectors/angleBetween/index.htm
-				(ac.angle() - bc.angle()) * (
-					if clockwise {1.} else {-1.}
-				)
+				ac.angle() - bc.angle()
 			} else {
 				// Based on jsdoom: https://github.com/pineapplemachine/jsdoom/blob/04593d2c30311212ccb9af9aa503cf5c83465655/src/convert/3DMapBuilder.ts#L172
 				// which was in itself based on what I learned after playing
@@ -70,14 +68,14 @@ fn angle_between(
 				let c = ac.cross(&bc);
 				cfg_if::cfg_if! {
 					if #[cfg(micromath)] {
-						F32(c).atan2(F32(d)).0 * if clockwise {-1.} else {1.}
+						F32(c).atan2(F32(d)).0
 					} else {
-						c.atan2(d) * if clockwise {-1.} else {1.}
+						c.atan2(d)
 					}
 				}
 			}
 		}
-	};
+	} * if clockwise {-1.} else {1.};
 
 	if ang < 0.0 {
 		ang + std::f32::consts::PI * 2.0
