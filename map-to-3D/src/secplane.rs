25b976c5d171605d8ffaf9c83e356b60a5856edd
Kevin Caccamo <kevin@ciinet.org>
2022-03-11 04:44:06 -0500

    Implement height_at for Sloped SectorPlane

--- map-to-3D/src/secplane.rs
+++ map-to-3D/src/secplane.rs
@@ -1,24 +1,30 @@
 use crate::vector::Vector2;
 
+#[derive(Debug, Clone)]
 enum SectorPlane {
 	Flat(f32),
 	// First number is the height, the other four make up the normal vector
 	// and distance from the "origin".
-	/*
 	Sloped {
-		h: f32,
 		a: f32,
 		b: f32,
 		c: f32,
 		d: f32
 	}
-	*/
 }
 
 impl SectorPlane {
 	fn height_at(&self, pos: &Vector2) -> f32 {
 		match self {
-			SectorPlane::Flat(height) => *height
+			SectorPlane::Flat(height) => *height,
+			SectorPlane::Sloped {
+				a, b, c, d
+			} => {
+				// https://github.com/Talon1024/jsdoom/blob/5c3ca7553b6579e26546781d1746d29819f5a784/src/convert/3DMapBuilder.ts#L650
+				let negative_c = -c;
+				let dividend = a * pos.x() + b * pos.y() + d;
+				dividend / negative_c
+			}
 		}
 	}
 }
