d1c0d4cf32199c0ea3f3736bc15b647e63b76166
Kevin Caccamo <kevin@ciinet.org>
2022-03-17 22:50:43 -0400

    Cleanup, and implementing basic movement

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -11,7 +11,10 @@ mod util;
 
 #[macroquad::main("Editor/viewer")]
 async fn main() {
-    let mut orientation = (std::f32::consts::PI / 2., 0.001953125);
+    use std::f32::consts::{PI, FRAC_PI_2};
+    let mouse_fac = 0.001953125;
+    let move_fac = 0.015625;
+    let mut orientation = (FRAC_PI_2, mouse_fac);
     let mut camera = Camera3D {
         position: Vec3::from((0.,0.,0.)),
         target: util::vec3_from_orientation(orientation),
@@ -25,6 +28,7 @@ async fn main() {
     let cube_mesh = sample_data::cube_mesh();
     let mut ptr_mode = PointerMode::Free;
     let mut last_mouse_pos = (0.0f32, 0.0f32);
+    let mut movement = Vec3::ZERO;
     loop {
         // Draw stuff
         clear_background(BEIGE);
@@ -41,6 +45,7 @@ async fn main() {
                 }
             },
             PointerMode::Locked => {
+                // Mouse looking
                 if is_key_pressed(KeyCode::Escape) {
                     ptr_mode = PointerMode::Free;
                     set_cursor_grab(false);
@@ -48,18 +53,31 @@ async fn main() {
                 }
                 let mouse_delta = mouse_position();
                 let mouse_delta = (
-                    (mouse_delta.0 - last_mouse_pos.0) * 0.001953125,
-                    (mouse_delta.1 - last_mouse_pos.1) * 0.001953125,
+                    (mouse_delta.0 - last_mouse_pos.0) * mouse_fac,
+                    (mouse_delta.1 - last_mouse_pos.1) * mouse_fac,
                 );
                 // println!("{} {}", mouse_delta.0, mouse_delta.1);
                 orientation.0 = (orientation.0 - mouse_delta.0)
-                    .rem_euclid(std::f32::consts::PI * 2.);
+                    .rem_euclid(PI * 2.);
                 orientation.1 = (orientation.1 + mouse_delta.1)
-                    .clamp(0.001953125, std::f32::consts::PI - 0.001953125);
+                    .clamp(mouse_fac, PI - mouse_fac);
+
+                // Forward, left, right, backward movement
+                movement = Vec3::new(
+                    if is_key_down(KeyCode::A) {-move_fac} else if is_key_down(KeyCode::D) {move_fac} else {0.},
+                    if is_key_down(KeyCode::W) {move_fac} else if is_key_down(KeyCode::S) {-move_fac} else {0.},
+                    0.,
+                );
             }
         }
-        let direction = util::vec3_from_orientation(orientation);
-        camera.target = direction;
+
+        // Apply changes caused by user inputs
+        let dir_vec = util::vec3_from_orientation(orientation);
+        let dir_quat =
+            Quat::from_rotation_arc(Vec3::Y, dir_vec);
+        movement = dir_quat.mul_vec3(movement);
+        camera.position += Vec3::from(movement);
+        camera.target = camera.position + dir_vec;
         camera.fovy = util::fov_x_to_y(100f32.to_radians());
         last_mouse_pos = mouse_position();
 
