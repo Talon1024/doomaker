061568452301f06741b61b23dd9d03fe4e34de83
Kevin Caccamo <kevin@ciinet.org>
2022-03-26 18:18:53 -0400

    Fix BoundingBox.is_inside

--- map-to-3D/src/boundingbox.rs
+++ map-to-3D/src/boundingbox.rs
@@ -18,9 +18,10 @@ impl BoundingBox {
 	pub fn is_inside(&self, vector: &Vector2) -> bool {
 		let x = vector.x();
 		let y = vector.y();
-		x > self.left && y < self.top &&
-			(x + self.left) < self.right() &&
-			(self.top - y) > self.bottom()
+		x >= self.left &&
+		y <= self.top &&
+		x <= self.right() &&
+		y >= self.bottom()
 	}
 }
 
@@ -38,8 +39,12 @@ mod tests {
 		let va = Vector2::new(7., 5.5);
 		let vb = Vector2::new(7., -8.);
 		let vc = Vector2::new(7., -10.5);
+		let vd = Vector2::new(4.5, -5.5);
+		let ve = Vector2::new(5.5, -5.5);
 		assert_eq!(bb.is_inside(&va), false);
 		assert_eq!(bb.is_inside(&vb), true);
 		assert_eq!(bb.is_inside(&vc), false);
+		assert_eq!(bb.is_inside(&vd), false);
+		assert_eq!(bb.is_inside(&ve), true);
 	}
 }
