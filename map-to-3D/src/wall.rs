533139dda26912b101e75810e3cb338d9af5b339
Kevin Caccamo <kevin@ciinet.org>
2022-04-22 18:45:49 -0400

    Rethink wall module
    I didn't put much thought into the module to begin with, but I'm planning on having it contain functions to calculate line quad geometry, like how the sectorpolygonbuilder module contains functions to calculate sector polygons

--- map-to-3D/src/wall.rs
+++ map-to-3D/src/wall.rs
@@ -1,18 +1,9 @@
 use crate::plane::Plane;
 use crate::edge::EdgeVertexIndex;
-use crate::vector::Vector2;
+use glam::Vec2;
 
 #[derive(Debug, Clone)]
 pub struct LineQuad {
-    a: Vector2,
-    b: Vector2,
-    width: f32,
-    height: f32,
-    tscale: Vector2,
-    upper: SectorPlane,
-    lower: SectorPlane,
-    colour_top: Option<[u8; 3]>,
-    colour_btm: Option<[u8; 3]>,
     quad_type: QuadType,
 }
 
@@ -21,43 +12,27 @@ enum QuadType {
     NormalQuad,
     OppositeTriangles,
     Vavoom3DFloor,
+    SkewedAtOneEnd, // Only for midtex quads
+    SkewedAtBothEnds,
 }
 
-impl LineQuad {
-    pub fn new(a: usize, b: usize, vertices: &[Vector2], sector: &Sector) -> LineQuad {
-        let mut positions: Vec<Vector3> = Vec::new();
-        let va = vertices[a];
-        let vb = vertices[b];
-        let upper = sector.upper.clone();
-        let lower = sector.lower.clone();
-        // "A/B upper/lower (absolute) height"
+pub fn solid_quad(va: Vec2, vb: Vec2, upper: Plane, lower: Plane, max_height: Option<f32>) -> LineQuad {
+    let mut positions: Vec<Vector3> = Vec::new();
+    // "A/B upper/lower (absolute) height"
+    let quad_type = {
         let auh = upper.z_at(va);
         let buh = upper.z_at(vb);
         let alh = lower.z_at(va);
         let blh = lower.z_at(vb);
-        let quad_type = {
-            if alh > auh && blh > buh {
-                QuadType::Vavoom3DFloor
-            } else if alh > auh || blh > buh {
-                QuadType::OppositeTriangles
-            } else {
-                QuadType::NormalQuad
-            }
-        };
-        LineQuad {
-            a: va,
-            b: vb,
-            width: (vb - va).length(),
-            height: 64.,
-            tscale: Vector2::new(1., 1.),
-            upper,
-            lower,
-            colour_top: sector.colour_top,
-            colour_btm: sector.colour_btm,
-            quad_type
+        if alh > auh && blh > buh {
+            QuadType::Vavoom3DFloor
+        } else if alh > auh || blh > buh {
+            QuadType::OppositeTriangles
+        } else {
+            QuadType::NormalQuad
         }
-    }
-    pub fn colour(&self) -> Vec<Vector3> {
-        //
+    };
+    LineQuad {
+        quad_type
     }
 }
