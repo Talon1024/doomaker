09a27a313c117b6ce7348db14f4f971d67c70a54
Kevin Caccamo <kevin@ciinet.org>
2022-04-23 12:55:24 -0400

    More work on the wall module

--- map-to-3D/src/wall.rs
+++ map-to-3D/src/wall.rs
@@ -1,10 +1,12 @@
 use crate::plane::Plane;
 use crate::edge::EdgeVertexIndex;
-use glam::Vec2;
+use glam::{Vec2, Vec3};
 
 #[derive(Debug, Clone)]
 pub struct LineQuad {
     quad_type: QuadType,
+    vertices: Box<[Vec3]>,
+    indices: Box<[u32]>,
 }
 
 #[derive(Debug, Clone)]
@@ -16,7 +18,25 @@ enum QuadType {
     SkewedAtBothEnds,
 }
 
-pub fn solid_quad(va: Vec2, vb: Vec2, upper: Plane, lower: Plane, max_height: Option<f32>) -> LineQuad {
+/// The minimum and maximum heights for a quad which does not span the full
+/// height between the top and bottom sector planes.
+#[derive(Debug, Clone, Copy)]
+pub struct HeightLimits {
+    pub top: f32,
+    pub bottom: f32
+}
+
+impl From<HeightLimits> for Option<HeightLimits> {
+    fn from(v: HeightLimits) -> Option<HeightLimits> {
+        if v.bottom < v.top {
+            Some(v)
+        } else {
+            None
+        }
+    }
+}
+
+pub fn calc_quad(va: Vec2, vb: Vec2, upper: Plane, lower: Plane, heights: Option<HeightLimits>) -> LineQuad {
     let mut positions: Vec<Vector3> = Vec::new();
     // "A/B upper/lower (absolute) height"
     let quad_type = {
