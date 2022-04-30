18d2cf359c4d4f101707cb4c81be54a1c0e14c8e
Kevin Caccamo <kevin@ciinet.org>
2022-04-30 18:08:30 -0400

    Add DIALOGUE to ALL_LUMPS

--- doom-wad/src/map/lumps.rs
+++ doom-wad/src/map/lumps.rs
@@ -4,4 +4,4 @@ pub(crate) const HEXEN_LUMPS: &str = "BEHAVIOR";
 pub(crate) const PSX_LUMPS: [&str; 2] = ["LEAFS", "LIGHTS"];
 pub(crate) const D64_LUMPS: [&str; 3] = ["LEAFS", "LIGHTS", "MACROS"];
 
-pub(crate) const ALL_LUMPS: [&str; 15] = ["THINGS", "LINEDEFS", "SIDEDEFS", "VERTEXES", "SECTORS", "SEGS", "SSECTORS", "NODES", "REJECT", "BLOCKMAP", "LEAFS", "LIGHTS", "MACROS", "BEHAVIOR", "SCRIPTS"];
+pub(crate) const ALL_LUMPS: [&str; 16] = ["THINGS", "LINEDEFS", "SIDEDEFS", "VERTEXES", "SECTORS", "SEGS", "SSECTORS", "NODES", "REJECT", "BLOCKMAP", "LEAFS", "LIGHTS", "MACROS", "BEHAVIOR", "SCRIPTS", "DIALOGUE"];
