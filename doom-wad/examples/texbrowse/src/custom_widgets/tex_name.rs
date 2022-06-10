e9424744cbeaa4c76f3690ba04bbe797e7942c52
Kevin Caccamo <kevin@ciinet.org>
2022-06-10 00:58:57 -0400

    Shorten texture names under more than 2 subdirectories

--- doom-wad/examples/texbrowse/src/custom_widgets/tex_name.rs
+++ doom-wad/examples/texbrowse/src/custom_widgets/tex_name.rs
@@ -4,6 +4,7 @@ use lazy_static::lazy_static;
 
 const MIN_BASE_CHARS: usize = 7;
 const MAX_BASE_CHARS: usize = 10;
+const MAX_DIR_LEVELS: usize = 2;
 
 #[derive(Debug, Clone)]
 pub struct TextureName<'a>(pub Cow<'a, str>);
@@ -12,6 +13,7 @@ impl<'a> TextureName<'a> {
 		lazy_static! {
 		static ref TEXNAME_NAME_R: Regex = Regex::new(r"/([^/]+)$").unwrap();
 		static ref TEXNAME_FOLDER_R: Regex = Regex::new(r"([^/]+)/").unwrap();
+		static ref TEXNAME_FOLDERS_R: Regex = Regex::new(r"([^/]{1,2}/)").unwrap();
 		}
 		let full_path = self.0.contains('/');
 		if !full_path {
@@ -21,13 +23,19 @@ impl<'a> TextureName<'a> {
 				format!("{}", self.0)
 			}
 		} else {
+			// STEP: Shorten directory names to one character and a dot
 			let text = TEXNAME_FOLDER_R.replace_all(&self.0, |caps: &Caps| {
+				let dir_name_len = caps[1].len();
 				let folder_first = (&caps[1]).chars().next().unwrap();
 				let mut dst = String::new();
 				dst.push(folder_first);
-				dst.push_str("./");
+				if dir_name_len > 1 {
+					dst.push('.');
+				}
+				dst.push('/');
 				dst
 			});
+			// STEP: Shorten pointlessly long file names
 			let text = TEXNAME_NAME_R.replace(&text, |caps: &Caps| {
 				let cap = &caps[1];
 				let mut extn = String::new();
@@ -44,7 +52,15 @@ impl<'a> TextureName<'a> {
 					format!("/{}", cap)
 				}
 			});
-			format!("{}", text)
+			// STEP: Shorten directories if there are more than three
+			let mut subdir_count = TEXNAME_FOLDERS_R.find_iter(&text).count();
+			if subdir_count > MAX_DIR_LEVELS {
+				subdir_count -= 1;
+				let text = TEXNAME_FOLDERS_R.replacen(&text, subdir_count, "");
+				format!("..{}", text)
+			} else {
+				text.to_string()
+			}
 		}
 	}
 }
@@ -79,5 +95,12 @@ mod tests {
 		let tex_name = "textures/studs/this_is_a_stupidly_and_pointlessly_long_texture_name_why_did_you_call_your_texture_this_stupidly_and_pointlessly_long_name_and_just_how_insane_do_you_have_to_be_to_do_something_like_this.png".to_string();
 		let tex_name = TextureName(Cow::from(&tex_name));
 		assert_eq!(tex_name.short_name(), "t./s./this_is..png".to_string())
+	}
+
+	#[test]
+	fn texture_name_subdirs() {
+		let tex_name = "textures/studs/alpha/beta/crap/deck.png".to_string();
+		let tex_name = TextureName(Cow::from(&tex_name));
+		assert_eq!(tex_name.short_name(), "..c./deck.png".to_string())
 	}
 }
