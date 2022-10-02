93a7461a88a390af00a02f86825f6c034230d13b
Kevin Caccamo <kevin@ciinet.org>
2022-10-02 11:03:22 -0400

    A couple more tests for Segment
    GeoGebra was helpful here

--- map-to-3D/src/segment.rs
+++ map-to-3D/src/segment.rs
@@ -5,6 +5,11 @@ use glam::Vec2;
 #[derive(Debug, Clone, Copy)]
 pub struct Segment(pub Vec2, pub Vec2);
 
+pub enum SegmentIntersection {
+	Normal(Vec2),
+	Collinear([Option<Vec2>; 2])
+}
+
 impl Segment {
 	pub fn intersection(&self, other: Segment) -> Option<Vec2> {
 		// Thanks to https://replit.com/@thehappycheese/linetools#LineTools/line_tools.py
@@ -17,9 +22,8 @@ impl Segment {
 		} else {
 			let ac = other.0 - self.0;
 			let ab_factor = ac.perp_dot(cd) / ab_cross_cd;
-			// Just in case
-			// let cd_factor = -ab.perp_dot(ac) / ab_cross_cd;
-			if ab_factor < 0. || ab_factor > 1. {
+			let cd_factor = -ab.perp_dot(ac) / ab_cross_cd;
+			if ab_factor < 0. || ab_factor > 1. || cd_factor < 0. || cd_factor > 1. {
 				None
 			} else {
 				Some(self.0 + ab * ab_factor)
@@ -71,5 +75,23 @@ mod tests {
 		let intersection_point = intersection_point.map(|v| format!("{:.3} {:.3}", v.x, v.y)).unwrap();
 		let expected = format!("{:.3} {:.3}", expected.x, expected.y);
 		assert_eq!(expected, intersection_point);
+	}
+
+	#[test]
+	fn intersection_d() {
+		// No intersection
+		let pa = Segment(Vec2::new(-2., -2.), Vec2::new(1., 3.));
+		let pb = Segment(Vec2::new(1., 1.), Vec2::new(4., -2.));
+		let intersection_point = pa.intersection(pb);
+		assert!(intersection_point.is_none());
+	}
+
+	#[test]
+	fn intersection_e() {
+		// Parallel segments
+		let pa = Segment(Vec2::new(0., 1.), Vec2::new(4., -2.));
+		let pb = Segment(Vec2::new(-3., -2.), Vec2::new(1., -5.));
+		let intersection_point = pa.intersection(pb);
+		assert!(intersection_point.is_none());
 	}
 }
